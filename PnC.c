4850d7243fc05b54f8f19d8523dce2ba7a28a2bd
wushanshan <747244019@qq.com>
2017-11-08 21:28:22 +0800

    fix some bug

--- PnC.c
+++ PnC.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
-#define Max 10
+#define Max 50
 pthread_mutex_t the_mutex;
 pthread_cond_t condC;
 pthread_cond_t condP;
-int bufferSize = 2;
+int bufferSize = 10;
 int buffer = 0;
+int total = 0;
 
 void *producer1(void *ptr)
 {
@@ -14,6 +16,9 @@ void *producer1(void *ptr)
 	for(int i = 0; i < Max; i++)
 	{
 		pthread_mutex_lock(&the_mutex);
+		if(total == 50){
+			exit(0);
+		}
 		while(buffer == bufferSize)
 		{
 			printf("The buffer is full! Producer1 is waiting.\n");		
@@ -34,6 +39,9 @@ void *producer2(void *ptr)
 	for(int i = 0; i < Max; i++)
 	{
 		pthread_mutex_lock(&the_mutex);
+		if(total == 50){
+			exit(0);
+		}
 		while(buffer == bufferSize)
 		{
 			printf("The buffer is full! Producer2 is waiting.\n");
@@ -54,6 +62,9 @@ void *producer3(void *ptr)
 	for(int i = 0; i < Max; i++)
 	{
 		pthread_mutex_lock(&the_mutex);
+		if(total == 50){
+			exit(0);
+		}
 		while(buffer == bufferSize)
 		{
 			printf("The buffer is full! Producer3 is waiting.\n");
@@ -74,12 +85,16 @@ void *consumer1(void *ptr)
 	for(int i =0; i < Max; i++)
 	{
 		pthread_mutex_lock(&the_mutex);
+		if(total == 50){
+			exit(0);
+		}
 		while(buffer == 0)
 		{
 			printf("The buffer is empty! Consumer1 is waiting.\n");
 			pthread_cond_wait(&condC, &the_mutex);
 		}
 		buffer--;
+		total++;
 		printf("Consumer1 retrived a pizza......%d\n", i);
 		sleep(1);
 		pthread_mutex_unlock(&the_mutex);
@@ -94,33 +109,17 @@ void *consumer2(void *ptr)
 	for(int i =0; i < Max; i++)
 	{
 		pthread_mutex_lock(&the_mutex);
+		if(total == 50){
+			exit(0);
+		}
 		while(buffer == 0)
 		{
 			printf("The buffer is empty! Consumer2 is waiting.\n");
 			pthread_cond_wait(&condC, &the_mutex);
 		}
 		buffer--;
+		total++;
 		printf("Consumer2 retrived a pizza......%d\n", i);
-		sleep(1);
-		pthread_cond_broadcast(&condC);
-		pthread_cond_broadcast(&condP);
-		pthread_mutex_unlock(&the_mutex);
-	}
-	pthread_exit(0);
-}
-void *consumer3(void *ptr)
-{
-	printf("con3\n");
-	for(int i =0; i < Max; i++)
-	{
-		pthread_mutex_lock(&the_mutex);
-		while(buffer == 0)
-		{
-			printf("The buffer is empty! Consumer3 is waiting.\n");
-			pthread_cond_wait(&condC, &the_mutex);
-		}
-		buffer--;
-		printf("Consumer3 retrived a pizza......%d\n", i);
 		sleep(1);
 		pthread_cond_broadcast(&condC);
 		pthread_cond_broadcast(&condP);
@@ -130,13 +129,10 @@ void *consumer3(void *ptr)
 }
 int main(int argc, char **argv)
 {
-	pthread_t pro1, con1, pro2, con2, pro3, con3;
+	pthread_t pro1, con1, pro2, con2, pro3;
 	pthread_mutex_init(&the_mutex, 0);
 	pthread_cond_init(&condC, 0);
 	pthread_cond_init(&condP, 0);
-	pthread_create(&con3, 0, consumer3, 0);
-	printf("Have create con3\n");
-	sleep(1);
 	pthread_create(&con2, 0, consumer2, 0);
 	printf("Have create con2\n");
 	sleep(1);
@@ -154,6 +150,7 @@ int main(int argc, char **argv)
 	//sleep(1);
 	pthread_join(pro1, 0);
 	pthread_join(pro2, 0);
+	pthread_join(pro3, 0);
 	pthread_join(con2, 0);
 	pthread_join(con1, 0);
 	pthread_cond_destroy(&condC);
